4bc0b26b1cc909c15025e9c8eb549a6f2c522740
lch09253 <lch09253@gmail.com>
2023-10-30 23:12:51 +0900

    feat: useRef

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const textRef = useRef();
+  const [text, setText] = useState("");
+
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleFocus = () => {
+    console.log("textRef", textRef);
+    textRef.current.focus();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>UseRef</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="input"
+          ref={textRef}
+          value={text}
+          onChange={onChangeText}
+        />
+        <button onClick={handleFocus}>클릭!</button>
+      </div>
+      <p>{text}</p>
+    </>
   );
 }
 
